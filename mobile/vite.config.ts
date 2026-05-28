8daf0ae7f3fc42285707b0965964bca7df72c85a
vamospoolid <admin@vamospool.id>
2026-05-29 03:25:08 +0800

    deploy: 2026-05-28 19:25:08

--- mobile/vite.config.ts
+++ mobile/vite.config.ts
@@ -17,8 +17,8 @@ export default defineConfig({
         background_color: '#2B35E8',
         display: 'standalone',
         orientation: 'portrait',
-        scope: '/',
-        start_url: '/',
+        scope: '/m/',
+        start_url: '/m/',
         icons: [
           { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
           { src: 'icon-512.png', sizes: '512x512', type: 'image/png' },
