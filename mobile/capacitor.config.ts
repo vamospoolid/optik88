58c5c6abd83225be25fd00a568446600b9da02ca
vamospoolid <admin@vamospool.id>
2026-05-29 05:34:07 +0800

    deploy: 2026-05-28 21:34:07

---
+++ mobile/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.optik88.pos',
+  appName: 'Optik88',
+  webDir: 'dist'
+};
+
+export default config;
